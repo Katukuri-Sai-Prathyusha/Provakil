2a744e7b6abb4ddfdab4bd03d83a6bb1c24ba6a2
Katukuri-Sai-Prathyusha <106062794+Katukuri-Sai-Prathyusha@users.noreply.github.com>
2023-02-22 16:31:26 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -13,5 +13,5 @@ def create_subscription(expiry_date,months_to_buy,monthly_cost):
         new_expiry = Enddate.strftime("%d/%m/%Y")
         cost = round(((Enddate-new_date).days)*(monthly_cost/30),2)
     return(new_expiry,cost);
-values = create_subscription("3/06/2022",3,400)
+values = create_subscription("19/06/2022",1,1000)
 print(values)
